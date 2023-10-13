bde1b7a861a1b548c04f7f1c64305e355bdda5f3
カール <138412347+calvinkbc04@users.noreply.github.com>
2023-10-13 15:07:04 +0900

    cppファイル終わり

---
+++ gameprogramming/Tetris1/GameMainScene.cpp
@@ -0,0 +1,75 @@
+#include "GameMainScene.h"
+#include "DxLib.h"
+#include "Block.h"
+#include "SceneManager.h"
+
+int BackGround_image;
+int BackGround_sound;
+int GameOver_sound;
+int Score;
+
+int GameMainScene_Initialize(void)
+{
+	int ret = 0;
+	
+	ret = Block_Initialize();
+	BackGround_image = LoadGraph("images/stage.png");
+	BackGround_sound = LoadSoundMem("sounds/BGM017.ogg");
+	GameOver_sound = LoadSoundMem("sounds/GameOver.mp3");
+
+	if (BackGround_image == -1)
+	{
+		ret = -1;
+	}
+	if (BackGround_sound == -1)
+	{
+		ret = -1;
+	}
+
+	return ret;
+}
+
+/*********************************************************
+	ゲームメイン画面　：　更新処理
+	引　数　：　なし
+	戻り値　：　なし
+**********************************************************/
+
+void GameMainScene_Update(void)
+{
+	//BGMの再生
+	PlaySoundMem(BackGround_sound, DX_PLAYTYPE_LOOP, FALSE);
+	
+	Block_Update();
+
+	Score = Get_Line() * 50;
+
+	if (Get_GenerateFlg() != TRUE)
+	{
+		PlaySoundMem(GameOver_sound, DX_PLAYTYPE_BACK, FALSE);
+		Change_Scene(E_RANKING);
+		StopSoundMem(BackGround_sound);
+	}
+}
+
+/*********************************************************
+	ゲームメイン画面　：　更新処理
+	引　数　：　なし
+	戻り値　：　なし
+**********************************************************/
+
+void GmaeMainScene_Draw(void)
+{
+	DrawGraph(0, 0, BackGround_Image, TRUE);
+
+	Block_Draw();
+	SetFontSize(100);
+
+	DrawFormatString(800, 100, GetColor(255, 255, 255), "%d", Score);
+	SetFontSize(20);
+}
+
+int Get_Score(void)
+{
+	return Score;
+}
---
+++ gameprogramming/Tetris1/InputControl.cpp
@@ -0,0 +1,134 @@
+#include "InputControl.h"
+#include "DxLib.h"
+
+/*********************************************************
+	マクロ定義
+**********************************************************/
+
+#define XINPUT_BUTTON_MAX	(16)
+
+/*********************************************************
+	型定義
+**********************************************************/
+
+enum KEY_STATE
+{
+	E_NONE,
+	E_CLICK,
+	E_PRESS,
+	E_RELEASED,
+	E_KEY_STATE_MAX
+};
+
+/*********************************************************
+	変数宣言
+**********************************************************/
+KEY_STATE button_state[XINPUT_BUTTON_MAX];
+
+/*********************************************************
+入力制限機能：初期化処理
+引　数：なし
+戻り値：なし
+**********************************************************/
+void InputControl_Initialize(void)
+{
+	int i;
+
+	for (i = 0; i < XINPUT_BUTTON_MAX; i++)
+	{
+		button_state[i] = E_NONE;
+	}
+}
+
+/*********************************************************
+	入力制御機能　：　更新処理
+	引　数　：　なし
+	戻り値　：　なし
+**********************************************************/
+void InputControl_Update(void)
+{
+	int i;
+	XINPUT_STATE input_controller;
+
+	GetJoypadInputState(DX_INPUT_PAD1, &input_controller);
+
+	for (i = 0; i < XINPUT_BUTTON_MAX; i++)
+	{
+		if (input_controller.Buttons[i] == TRUE)
+		{
+			switch (button_state[i])
+			{
+			case E_NONE:
+			case E_RELEASED:
+				button_state[i] = E_CLICK;
+				break;
+			case E_CLICK:
+			case E_PRESS:
+				button_state[i] = E_PRESS;
+				break;
+			default:
+				button_state[i] = E_NONE;
+				break;
+			}
+		}
+		else
+		{
+			switch (button_state[i])
+			{
+			case E_NONE:
+			case E_RELEASED:
+				button_state[i] = E_NONE;
+				break;
+			case E_CLICK:
+			case E_PRESS:
+				button_state[i] = E_RELEASED;
+				break;
+			default:
+				button_state[i] = E_NONE;
+				break;
+			}
+		}
+	}
+}
+
+int GetButton(int button)
+{
+	int ret = FALSE;
+	if (button_state[button] == E_CLICK || button_state[button] == E_PRESS)
+	{
+		ret = TRUE;
+	}
+	return ret;
+}
+
+int GetButtonDown(int button)
+{
+	int ret = FALSE;
+	if (button_state[button] == E_CLICK)
+	{
+		ret = TRUE;
+	}
+
+	return ret;
+}
+
+int GetButtonUp(int button)
+{
+	int ret = FALSE;
+	if (button_state[button] == E_RELEASED)
+	{
+		ret = TRUE;
+	}
+	return ret;
+}
+
+int GetExitButton(void)
+{
+	int ret = FALSE;
+
+	if ((GetButtonDown(XINPUT_BUTTON_BACK) == TRUE) || (CheckHitKey(KEY_INPUT_ESCAPE) == TRUE))
+	{
+		ret = TRUE;
+	}
+	return ret;
+}
